9da47c21fa54fd00137dcca933e5426fe424fb0a
AngelFireLA <tinyangel@mail.com>
2025-06-28 11:35:14 +0200

    finish adding minion

--- add_minion.py
+++ add_minion.py
@@ -1,17 +1,37 @@
 from PIL import Image
 import zlib
+import subprocess
+# Need to have been extracted using the extract_from_swf.bat script.
 minion_dex_id_path = "source\scripts\States\MinionDexID.as"
 all_minions_container_path = "source\scripts\Minions\AllMinionsContainer.as"
 symbol_classes_path = "source\symbolClass\symbols.csv"
-new_minion_image_path = r"C:\Dev\basard\Min Hero\MinHeroMods\eevee_minion.png"
 images_folder_path = "source\images"
 
+# Path to your minion's image
+new_minion_image_path = r"C:\Dev\basard\Min Hero\MinHeroMods\eevee_minion.png"
+
+# Unmodified SWF 
+base_swf_path = "original.swf"
+
+# Final SWF after modifications
+final_swf_path = "default.swf"
+
+intermediary_xml_path = "dump.xml"
+
+# Path to the modified file detector script so we don't recompile every script every time
+modified_file_detector_script_path = "modified_detector.py"
+
 def add_dex_id(minion_code_name):
+    # We need to add the minion to the DEX IDs file, but it can't just be the last one, it needs to be before the 4 not-really-minions.
     with open(minion_dex_id_path, "r") as f:
         content = f.read()
 
+        # We get the first line that contains the start of the not-really-minions
         line_to_replace = next(line for line in content.splitlines() if "public static const DEX_ID_battleModMinion_1:int =" in line)
+        # We get the number from the line, and add 1 to it, because the original number will be the ID of our new minion
         first_num = int(line_to_replace.split("=")[1].strip().rstrip(";"))
+
+        # We replace the 4 not-really-minions lines with their old numbers + 1
         new_line_to_replace =  f"      public static const DEX_ID_battleModMinion_1:int = {first_num+1};"
         content = content.replace(line_to_replace, new_line_to_replace)
 
@@ -27,15 +47,21 @@ def add_dex_id(minion_code_name):
         new_line_to_replace =  f"      public static const DEX_ID_testing_minion:int = {first_num+4};"
         content = content.replace(line_to_replace, new_line_to_replace)
 
+        # We also edit the total number of minions
         total_num_line = next(line for line in content.splitlines() if "public static const TOTAL_NUM_OF_MINIONS:int =" in line)
         minion_amount_line =  f"      public static const TOTAL_NUM_OF_MINIONS:int = {first_num+5};"
+
+        # We add our new minion line
         new_minion_line = f"      public static const DEX_ID_{minion_code_name}:int = {first_num};"
         content = content.replace(total_num_line, f"{new_minion_line}\n\n{minion_amount_line}")
         
     with open(minion_dex_id_path, "w") as f:
         f.write(content)
 
+
 def add_minion_container(minion_code_name:str, minion_name:str, icon_offset_x:int, icon_offset_y:int, exp_gain_rate:str, number_of_gems:int, starting_moves_ids:list, base_health:int, base_energy:int, base_attack:int, base_healing:int, base_speed:int, specialized_move_ids:list, minion_type1:str, minion_type2: str = None):
+    
+    # Constants so we don't have to remember the exact strings
     exp_gain_rates = {"very easy":"ExpGainRates.EXP_GAIN_RATE_VERY_EASY", "easy":"ExpGainRates.EXP_GAIN_RATE_EASY", "normal":"ExpGainRates.EXP_GAIN_RATE_NORMAL", "hard":"ExpGainRates.EXP_GAIN_RATE_HARD", "very hard":"ExpGainRates.EXP_GAIN_RATE_VERY_HARD"}
     minion_types = {
         "energy": "TYPE_ENERGY",
@@ -53,6 +79,8 @@ def add_minion_container(minion_code_name:str, minion_name:str, icon_offset_x:in
         "normal": "TYPE_NORMAL",
         "dino": "TYPE_DINO"
     }
+    
+    # We check that the arguments are mostly valid
     if exp_gain_rate not in exp_gain_rates:
         raise ValueError(f"Exp Gain rate {exp_gain_rate} for minion {minion_code_name} isn't valid.")
     if minion_type1 not in minion_types:
@@ -62,9 +90,16 @@ def add_minion_container(minion_code_name:str, minion_name:str, icon_offset_x:in
     if len(specialized_move_ids) != 3:
         raise ValueError(f"You must have exactly 3 specialized moves for minion {minion_code_name}. (Currently {len(specialized_move_ids)})")
 
+    # We replace the simple string with the code string
     exp_gain_rate = exp_gain_rates[exp_gain_rate]
+
+    # We handle the cases where there are more than one starting move
     starting_moves_line = "\n".join([f"         _loc1_.AddStartingMove({move_id}); " for move_id in starting_moves_ids])
+    
+    # We have to handle the cases where the minions has either 1 or two types
     minion_type_str = f"MinionType.{minion_types[minion_type1]}" if not minion_type2 else f"MinionType.{minion_types[minion_type1]},MinionType.{minion_types[minion_type2]}"
+    
+    
     minion_code_block = f"""
       private function {minion_code_name}() : void
       {{
@@ -113,9 +148,6 @@ def add_minion_container(minion_code_name:str, minion_name:str, icon_offset_x:in
         f.write(content)
 
 
-
-    #private function CM(param1:int, param2:String, param3:String, param4:int, param5:int, param6:int, param7:int, param8:int, param9:int, param10:int = 0) : BaseMinion
-
 def add_image(image_path, minion_code_name):
 
     # open the symbols.csv file and get the last value of the first column
@@ -163,10 +195,12 @@ package Utilities
                 break
     with open(main_sprite_handler_path, "w") as f:
         f.writelines(lines)
-
+    print("Starting swf2xml...")
+    subprocess.run(fr'"jpexs\ffdec-cli.exe" -swf2xml {base_swf_path} {intermediary_xml_path}', shell=True)
+    print("SWF to XML conversion completed.")
     # open dump.xml
-    dump_xml_path = "dump.xml"
-    with open(dump_xml_path, "r") as f:
+
+    with open(intermediary_xml_path, "r") as f:
         dump_lines = f.readlines()
 
     # find the first line containing '<item type="DefineBitsLossless2Tag" bitmapColorTableSize="0"'
@@ -193,30 +227,153 @@ package Utilities
     # insert the image_string on the line just before the define_bits_index
     dump_lines.insert(define_bits_index, image_string)
 
-    # find all the indices where </tags> appears
     closing_tag_indices = [i for i, line in enumerate(dump_lines) if "</tags>" in line]
-    # the 3rd occurrence is at index 2 in that list
     third_index = closing_tag_indices[2]
-    # insert your new <item> line just above it
     dump_lines.insert(third_index, f"        <item>{new_index}</item>\n")
-
-    # find all the indices where </names> appears
     name_tag_indices = [i for i, line in enumerate(dump_lines) if "</names>" in line]
     last_index = name_tag_indices[-1]
-    # insert your new <item> line just above it
     dump_lines.insert(last_index, f"        <item>Utilities.SpriteHandler_{minion_code_name}</item>\n")
 
+    string_to_add = f"""
+    <item type="DoABC2Tag" flags="0" forceWriteAsLong="false" name="Utilities.SpriteHandler_{minion_code_name}">
+      <abc type="ABC">
+        <version type="ABCVersion" major="46" minor="16"/>
+        <constants type="AVM2ConstantPool">
+          <constant_int/>
+          <constant_uint/>
+          <constant_double/>
+          <constant_decimal/>
+          <constant_float/>
+          <constant_float4/>
+          <constant_string>
+            <item isNull="true"/>
+            <item>http://adobe.com/AS3/2006/builtin</item>
+            <item>Utilities</item>
+            <item>SpriteHandler_{minion_code_name}</item>
+            <item>Object</item>
+            <item/>
+            <item>Utilities:SpriteHandler_{minion_code_name}</item>
+          </constant_string>
+          <constant_namespace>
+            <item isNull="true"/>
+            <item type="Namespace" kind="8" name_index="1"/>
+            <item type="Namespace" kind="22" name_index="2"/>
+            <item type="Namespace" kind="22" name_index="5"/>
+            <item type="Namespace" kind="5" name_index="6"/>
+            <item type="Namespace" kind="24" name_index="6"/>
+          </constant_namespace>
+          <constant_namespace_set>
+            <item isNull="true"/>
+            <item type="NamespaceSet">
+              <namespaces>
+                <item>2</item>
+              </namespaces>
+            </item>
+          </constant_namespace_set>
+          <constant_multiname>
+            <item isNull="true"/>
+            <item type="Multiname" kind="7" name_index="3" namespace_index="2" namespace_set_index="0" qname_index="0"/>
+            <item type="Multiname" kind="7" name_index="4" namespace_index="3" namespace_set_index="0" qname_index="0"/>
+            <item type="Multiname" kind="9" name_index="3" namespace_index="0" namespace_set_index="1" qname_index="0"/>
+          </constant_multiname>
+        </constants>
+        <method_info>
+          <item type="MethodInfo" flags="0" name_index="5" ret_type="0">
+            <param_types/>
+            <optional/>
+            <paramNames/>
+          </item>
+          <item type="MethodInfo" flags="0" name_index="0" ret_type="0">
+            <param_types/>
+            <optional/>
+            <paramNames/>
+          </item>
+          <item type="MethodInfo" flags="0" name_index="5" ret_type="0">
+            <param_types/>
+            <optional/>
+            <paramNames/>
+          </item>
+        </method_info>
+        <metadata_info/>
+        <instance_info>
+          <item type="InstanceInfo" flags="9" iinit_index="1" name_index="1" protectedNS="5" super_index="2">
+            <interfaces/>
+            <instance_traits type="Traits">
+              <traits/>
+            </instance_traits>
+          </item>
+        </instance_info>
+        <class_info>
+          <item type="ClassInfo" cinit_index="2">
+            <static_traits type="Traits">
+              <traits/>
+            </static_traits>
+          </item>
+        </class_info>
+        <script_info>
+          <item type="ScriptInfo" init_index="0">
+            <traits type="Traits">
+              <traits>
+                <item type="TraitClass" class_info="0" deleted="false" fileOffset="0" kindFlags="0" kindType="4" name_index="1" slot_id="1">
+                  <metadata/>
+                </item>
+              </traits>
+            </traits>
+          </item>
+        </script_info>
+        <bodies>
+          <item type="MethodBody" init_scope_depth="1" max_regs="1" max_scope_depth="3" max_stack="2" method_info="0">
+            <exceptions/>
+            <traits type="Traits">
+              <traits/>
+            </traits>
+          </item>
+          <item type="MethodBody" init_scope_depth="4" max_regs="1" max_scope_depth="5" max_stack="1" method_info="1">
+            <exceptions/>
+            <traits type="Traits">
+              <traits/>
+            </traits>
+          </item>
+          <item type="MethodBody" init_scope_depth="3" max_regs="1" max_scope_depth="4" max_stack="1" method_info="2">
+            <exceptions/>
+            <traits type="Traits">
+              <traits/>
+            </traits>
+          </item>
+        </bodies>
+      </abc>
+    </item>
+"""
+    idk = [i for i, line in enumerate(dump_lines) if '<item type="ExportAssetsTag" forceWriteAsLong="true">' in line]
+    last_index = idk[0]
+    # add the string_to_add before the last index
+    dump_lines.insert(last_index, string_to_add)
 
-    with open(dump_xml_path, "w") as f:
+
+    with open(intermediary_xml_path, "w") as f:
         f.writelines(dump_lines)
+
+    print("Starting xml2swf...")
+    subprocess.run(fr'"jpexs\ffdec-cli.exe" -xml2swf {intermediary_xml_path} {final_swf_path}', shell=True)
+    print("SWF to XML conversion completed.")
+
+    print("Starting modified file detector script...")
+    subprocess.run(fr'python {modified_file_detector_script_path}', shell=True)
+    print("Modified file detector script completed.")
+
+    # swf2swf
+    print("Starting importing scripts into swf file...")
+    subprocess.run(fr'"jpexs\ffdec-cli.exe" -importScript {final_swf_path} {final_swf_path} modified', shell=True)
+    print("Scripts imported successfully.")
+
 
 
 def add_minion():
     add_dex_id("eeveeMinion")
-    add_minion_container("eeveeMinion", "Eevee", 0, 0, "normal", 4, [1, 2, 3], 100, 100, 100, 100, 100, [4, 5, 6], "water")
+    add_minion_container("eeveeMinion", "Eevee", 0, 0, "normal", 4, [1, 4, 7], 100, 100, 100, 100, 100, [8, 5, 6], "water")
     add_image(new_minion_image_path, "eeveeMinion")
+    print("Minion added successfully. Please check the output for any errors.")
 
 
 if __name__ == "__main__":
     add_minion()
-    print("Minion added successfully.")
